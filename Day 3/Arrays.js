17f90813235a346d9afdc71aec79f69519337de5
github username <adisagir@gmail.com>
2019-11-12 19:08:02 -0500

    Completed

---
+++ Day 3/Arrays.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString.trim().split('\n').map(string => {
+    return string.trim();
+  });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+function getSecondLargest(nums) {
+  // Complete the function
+
+  // First, get the largest number
+  let max = Math.max(...nums);
+
+  // Filter out the largest number
+  nums = nums.filter(i => i !== max);
+
+  // Now return the new largest number
+  return Math.max(...nums);
+}
